8d707f51616aa5ca12e4b61daa4b800a060e5acb
Abby <abigailjwinn@gmail.com>
2023-06-20 14:34:51 -0500

    progress on component tree

--- src/components/coaches/coaches.js
+++ src/components/coaches/coaches.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Grid } from '@mui/material';
-import GenericModal from './modal-component';
 import Table2 from './table';
+import {
+  ArchiveCoachModal,
+  GenericModal,
+  AddCoachModal,
+} from './modal-component';
 
 export function Coaches() {
   const actionButtonFunction = () => {
@@ -17,34 +21,8 @@ export function Coaches() {
         </Grid>
       </Grid>
 
-      <GenericModal
-        usingFields
-        openModal="Register a Coach"
-        modalHeadingTitle="Create a Coach"
-        modalMessage="Create a username and password"
-        actionButtonColor="submit"
-        actionButtonTitle="Register"
-        cancelButtonTitle="Cancel"
-      />
-      <GenericModal
-        usingDropDown
-        openModal="Select a Coach"
-        modalHeadingTitle="Select a Coach"
-        modalMessage="Select a coach to assign this student to."
-        actionButtonTitle="Select"
-        actionButtonColor="submit"
-        cancelButtonTitle="cancel"
-      />
-      <GenericModal
-        usingTwoButtonFormat
-        openModal="Archive"
-        modalHeadingTitle="Archive a Coach"
-        modalMessage="Are you sure you want to archive this coach?"
-        actionButtonTitle="Archive"
-        cancelButtonTitle="cancel"
-        actionButtonFunction={actionButtonFunction}
-        actionButtonColor="archive"
-      />
+      <AddCoachModal />
+      <ArchiveCoachModal />
     </div>
   );
 }
--- src/components/coaches/modal-component.js
+++ src/components/coaches/modal-component.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Children } from 'react';
 import {
   Box,
   Button,
@@ -6,47 +6,21 @@ import {
   Modal,
   Grid,
   IconButton,
+  Stack,
+  TextField,
 } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import PropTypes from 'prop-types';
-import ModalMessageComponent from './modal-message';
-import TwoButtonComponent from './two-button-component';
-import FieldComponent from './field-component';
-import { DropDownComponent } from './drop-down-component';
-
-// Background of modal styling:
-
-const backgroundStyle = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 475,
-  bgcolor: 'background.paper',
-  boxShadow: 24,
-  borderRadius: '10px',
-};
-
-// Heading styling:
-const headingStyle = {
-  bgcolor: '#3E4C61',
-  color: 'white',
-  borderTopLeftRadius: 10,
-  borderTopRightRadius: 10,
-  boxShadow: 4,
-};
-
-const headingText = {
-  fontSize: 29,
-};
-
-// "X" icon styling:
-const closeIconStyle = {
-  color: '#C5C5C5',
-  position: 'absolute',
-  right: 8,
-  top: 8,
-};
+import {
+  backgroundStyle,
+  headingStyle,
+  headingText,
+  closeIconStyle,
+  footerStyle,
+  buttonBackground,
+  buttonText,
+  buttonTheme,
+} from './modal-styling';
 
 export function GenericModal(props) {
   const {
@@ -56,10 +30,8 @@ export function GenericModal(props) {
     actionButtonFunction,
     actionButtonTitle,
     cancelButtonTitle,
-    usingTwoButtonFormat,
-    usingFields,
     actionButtonColor,
-    usingDropDown,
+    children,
   } = props;
 
   const [open, setOpen] = React.useState(false);
@@ -79,50 +51,116 @@ export function GenericModal(props) {
               <CloseIcon fontSize="large" sx={closeIconStyle} />
             </IconButton>
           </Grid>
-          <ModalMessageComponent modalMessage={modalMessage} />
-          {usingFields && (
-            <FieldComponent
-              handleClose={handleClose}
-              actionButtonFunction={actionButtonFunction}
-              actionButtonTitle={actionButtonTitle}
-              cancelButtonTitle={cancelButtonTitle}
-              actionButtonColor={actionButtonColor}
-            />
-          )}
-          {usingTwoButtonFormat && (
-            <TwoButtonComponent
-              handleClose={handleClose}
-              actionButtonFunction={actionButtonFunction}
-              actionButtonTitle={actionButtonTitle}
-              cancelButtonTitle={cancelButtonTitle}
-              actionButtonColor={actionButtonColor}
-            />
-          )}
-          {usingDropDown && (
-            <DropDownComponent
-              handleClose={handleClose}
-              actionButtonFunction={actionButtonFunction}
-              actionButtonTitle={actionButtonTitle}
-              cancelButtonTitle={cancelButtonTitle}
-              actionButtonColor={actionButtonColor}
-            />
-          )}
+          <Typography padding={5} align="center" fontSize={20}>
+            {modalMessage}
+          </Typography>
+
+          {children}
+
+          <Grid item sx={footerStyle} xs={12}>
+            <Stack
+              direction="row"
+              spacing={0}
+              justifyContent="right"
+              padding={3}
+            >
+              <Button
+                variant="text"
+                onClick={handleClose}
+                spacing={2}
+                sx={buttonBackground}
+                theme={buttonTheme}
+                color="cancel"
+              >
+                <Typography style={buttonText}>{cancelButtonTitle}</Typography>
+              </Button>
+              <Button
+                variant="contained"
+                onClick={() => {
+                  actionButtonFunction();
+                  handleClose();
+                }}
+                sx={buttonBackground}
+                theme={buttonTheme}
+                color={actionButtonColor}
+              >
+                <Typography style={buttonText}>{actionButtonTitle}</Typography>
+              </Button>
+            </Stack>
+          </Grid>
         </Box>
       </Modal>
     </div>
   );
 }
 
+GenericModal.defaultProps = {
+  openModal: null,
+};
+
 GenericModal.propTypes = {
-  openModal: PropTypes.string.isRequired,
+  openModal: PropTypes.string,
   modalHeadingTitle: PropTypes.string.isRequired,
   modalMessage: PropTypes.string.isRequired,
   actionButtonFunction: PropTypes.element.isRequired,
   actionButtonTitle: PropTypes.string.isRequired,
   cancelButtonTitle: PropTypes.string.isRequired,
-  usingTwoButtonFormat: PropTypes.bool.isRequired,
-  usingFields: PropTypes.bool.isRequired,
   actionButtonColor: PropTypes.string.isRequired,
 };
+
+export function AddCoachModal() {
+  const addCoachAction = () => {
+    console.log('here is where you connect to api');
+  };
+
+  return (
+    <div>
+      <GenericModal
+        openModal="open1"
+        modalHeadingTitle="Add a Coach"
+        modalMessage="Fill out the fields below to add a coach."
+        actionButtonTitle="Create"
+        cancelButtonTitle="Cancel"
+        actionButtonColor="submit"
+        actionButtonFunction={addCoachAction}
+      >
+        <Grid container justifyContent="center">
+          {' '}
+          {/* Center the Grid container */}
+          <Grid item xs={7}>
+            <TextField
+              fullWidth // Make the TextField component occupy the full width
+              error
+              helperText="Username cannot include a space."
+            />
+          </Grid>
+        </Grid>
+        <TextField fullWidth />{' '}
+        {/* Make the TextField component occupy the full width */}
+        <TextField />
+      </GenericModal>
+    </div>
+  );
+}
+
+export function ArchiveCoachModal() {
+  const archiveCoachAction = () => {
+    console.log('here is where you connect to api');
+  };
+
+  return (
+    <div>
+      <GenericModal
+        openModal="open"
+        modalHeadingTitle="Archive Coach"
+        modalMessage="Are you sure you want to archive this coach?"
+        actionButtonTitle="Archive"
+        cancelButtonTitle="Cancel"
+        actionButtonColor="archive"
+        actionButtonFunction={archiveCoachAction}
+      />
+    </div>
+  );
+}
 
 export default GenericModal;
