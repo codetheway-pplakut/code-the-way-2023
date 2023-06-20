9a90d26da4247f749dcbac520b947756a9674bc3
Adam Miller <theadammiller56@gmail.com>
2023-06-20 14:42:53 -0500

    Coach Past API Calls
    API Calls from last year for Coaches.

--- src/components/coaches/coaches.js
+++ src/components/coaches/coaches.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import { getCoaches } from '../../services/coaches/coaches';
+import {
+  addCoach,
+  getCoachById,
+  getCoaches,
+} from '../../services/coaches/coaches';
 import { TableLayoutWithRequest } from '../table-layout-with-request/table-layout-with-request';
+import { editStudent } from '../../services/students/students';
 
 const COLUMNS = [
   {
@@ -24,6 +29,60 @@ const COLUMNS = [
     width: 300,
   },
 ];
+
+/**
+ * Returns all coaches in the DB
+ * @returns All Coaches
+ * @author Adam Miller
+ */
+export function getCoachesHandler() {
+  return getCoaches();
+}
+
+/**
+ * Gets all info from the DB.
+ *
+ * @param {uuid} coachId Coach to get info on.
+ * @returns {{id:uuid, coachFirstName: string, coachLastName:string, coachEmail: string, coachPhoneNumber:string, students:[{}]}}
+ * @author Adam Miller
+ */
+export function getCoachByIdHandler(coachId) {
+  return getCoachById(coachId);
+}
+
+/**
+ * Adds a Coach to the DB
+ *
+ * @param {string} firstName
+ * @param {string} lastName
+ * @param {email} email
+ * @param {string} phone
+ * @author Adam Miller
+ */
+export function addCoachHandler(firstName, lastName, email, phone) {
+  const data = { firstName, lastName, email, phone };
+  addCoach(data);
+}
+
+/**
+ * Edits a coach, replacing the whole DB entry.
+ *
+ * @param {Coach} coach Whole coach object, including students, to change.
+ * @author Adam Miller
+ */
+export function editCoachHandler(coach) {
+  editStudent(coach);
+}
+
+/**
+ * Deletes a coach from the DB. THIS SHOULD NOT BE USED.
+ *
+ * @param {uuid} coachId
+ * @author Adam Miller
+ */
+export function deleteCoach(coachId) {
+  deleteCoach(coachId);
+}
 
 export function Coaches() {
   return (
--- src/services/coaches/coaches.js
+++ src/services/coaches/coaches.js
@@ -1,3 +1,65 @@
 import { callApi } from '../../utils/call-api/call-api';
 
+/**
+ * Gets all coaches in the DB
+ * @returns ALl Coaches in DB
+ * @author Joey Schroeder
+ */
 export const getCoaches = () => callApi({ url: '/Coaches' });
+
+/**
+ * Gets all data on a coach, including all students (same as getCoaches at the moment. To be deprecated.)
+ * @param {uuid} coachId
+ * @returns {{id:uuid, coachFirstName: string, coachLastName:string, coachEmail: string, coachPhoneNumber:string, students:[{}]}}
+ * @author Adam Miller
+ */
+export const getCoachById = (coachId) =>
+  callApi({ url: `/Coaches/${coachId}` });
+/**
+ * Adds a coach to the DB
+ * @param {{firstName: string, lastName:string, email:email, phone: string}} coach - Coach to add to DB
+ * @author Adam Miller
+ */
+export const addCoach = (coach) =>
+  callApi({
+    url: '/Coaches',
+    data: coach,
+    method: 'POST',
+    headers: {
+      Accept: 'application/json, text/plain',
+      'Content-Type': 'application/json',
+    },
+  });
+
+/**
+ * Edits a Coach, replacing entry in DB
+ *
+ * @param {{id:uuid, coachFirstName: string, coachLastName:string, coachEmail: string, coachPhoneNumber:string, students:[{}]}} coach
+ * @author Adam Miller
+ */
+export const editCoach = (coach) =>
+  callApi({
+    url: '/Coaches',
+    data: coach,
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json, text/plain',
+      'Content-Type': 'application/json',
+    },
+  });
+
+/**
+ * Deletes a coach from the DB. This should not be used, except for cleaning the DB.
+ * @param {uuid} coachId
+ * @author Adam Miller
+ */
+export const deleteCoach = (coachId) =>
+  callApi({
+    url: '/Coaches',
+    params: coachId,
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json, text/plain',
+      'Content-Type': 'application/json',
+    },
+  });
