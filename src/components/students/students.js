fbec8fd774bd819bffb585cca263ed6059b01e38
Adam Miller <theadammiller56@gmail.com>
2023-06-20 12:13:35 -0500

    API-PoppedStash

--- src/components/students/students.js
+++ src/components/students/students.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { getStudents } from '../../services/students/students';
+import { Button } from '@mui/material';
+import {
+  addStudent,
+  deleteStudent,
+  getStudents,
+} from '../../services/students/students';
 import { TableLayoutWithRequest } from '../table-layout-with-request/table-layout-with-request';
 
 const COLUMNS = [
@@ -33,15 +38,50 @@ const COLUMNS = [
   },
 ];
 
+/**
+ * Function to call API to add student.
+ *
+ * @param {string} firstName
+ * @param {string} lastName
+ * @param {string} email
+ * @param {Date} dateOfBirth
+ * @param {string} cellPhone
+ * @author Adam Miller
+ *
+ */
+export function addStudentHandler(
+  firstName,
+  lastName,
+  email,
+  dateOfBirth,
+  cellPhone
+) {
+  const data = { firstName, lastName, email, dateOfBirth, cellPhone };
+  addStudent(data);
+}
+
+/**
+ * Function to call API to delete student. This should not be used - We want to deactivate students instead.
+ * @param {uuid} studentId - Id of student to delete
+ * @author Adam Miller
+ */
+export function deleteStudentHandler(studentId) {
+  const params = { id: studentId };
+  deleteStudent(params);
+}
+
 export function Students() {
   return (
-    <TableLayoutWithRequest
-      columns={COLUMNS}
-      tableSize="small"
-      requestFunc={getStudents}
-      requestLabel="Request Students"
-      subTitle="View all students"
-      title="Students"
-    />
+    <div>
+      <Button>Test</Button>
+      <TableLayoutWithRequest
+        columns={COLUMNS}
+        tableSize="small"
+        requestFunc={getStudents}
+        requestLabel="Request Students"
+        subTitle="View all students"
+        title="Students"
+      />
+    </div>
   );
 }
--- src/services/students/students.js
+++ src/services/students/students.js
@@ -1,3 +1,42 @@
 import { callApi } from '../../utils/call-api/call-api';
 
-export const getStudents = () => callApi({ url: '/Students' });
+export const getStudents = async () => callApi({ url: '/Students' });
+
+export const getStudentById = (id) =>
+  callApi({
+    url: `/Students/${id}`,
+  });
+
+// Takes in firstName, lastName, dateOfBirth, cellPhone, and email
+export const addStudent = async (student) =>
+  callApi({
+    url: '/Students',
+    data: student,
+    method: 'POST',
+    headers: {
+      Accept: 'application/json, text/plain',
+      'Content-Type': 'application/json',
+    },
+  });
+
+export const editStudent = async (student) =>
+  callApi({
+    url: '/Students',
+    data: student,
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json, text/plain',
+      'Content-Type': 'application/json',
+    },
+  });
+
+export const deleteStudent = async (studentId) =>
+  callApi({
+    url: '/Students',
+    params: studentId,
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json, text/plain',
+      'Content-Type': 'application/json',
+    },
+  });
