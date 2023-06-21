7f86968866eea83567030aea5f73666905e361f4
Aniket Singh <singani@hamiltonstudents.org>
2023-06-21 10:38:22 -0500

    fix: Scroll Button and Tab Styling

--- src/components/student-info/rowSpacing.js
+++ src/components/student-info/rowSpacing.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
-import { Box, IconButton, Paper, Tab, Tabs, Toolbar } from '@mui/material';
+import { Box, IconButton, Paper, Tab, Toolbar } from '@mui/material';
+import Tabs, { tabsClasses } from '@mui/material/Tabs';
 import AddIcon from '@mui/icons-material/Add';
 import GenericModal from '../coaches/modal-component';
 import InfoBox from './info-box';
@@ -12,9 +13,9 @@ export default function RowAndColumnSpacing() {
   };
 
   const style = {
-    flexShrink: 1,
-    display: 'inline-flex',
-    overflow: 'auto',
+    // flexShrink: 1,
+    // display: 'inline-flex',
+    // overflow: 'auto',
   };
   const tabStyle = {
     bgcolor: '#004cbb',
@@ -24,7 +25,7 @@ export default function RowAndColumnSpacing() {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    minWidth: 150,
+    // minWidth: 150,
     // '&:hover': {
     //   bgcolor: '#ffb570',
     //   opacity: 1,
@@ -49,13 +50,24 @@ export default function RowAndColumnSpacing() {
     <Grid container direction="column" alignItems="center">
       <Grid item container direction="row" alignItems="center" minWidth="100%">
         <Grid item width="50%" alignItems="center" mr="5%">
-          <Grid container direction="row" sx={style} alignItems="center">
+          <Grid
+            container
+            direction="row"
+            sx={style}
+            alignItems="center"
+            ml="10px"
+          >
             <Tabs
               value={value}
               onChange={handleChange}
               variant="scrollable"
-              scrollButtons="true"
-              width="100%"
+              scrollButtons="auto"
+              sx={{
+                width: '700px',
+                [`& .${tabsClasses.scrollButtons}`]: {
+                  '&.Mui-disabled': { opacity: 0.3 },
+                },
+              }}
             >
               <Tab sx={tabStyle} value="one" label="Contact Info" />
 
