adfed3403db1c4a4f69d695bef9f9a96dbd0fe4d
Joey Schroeder <jschroeder@artisanpartners.com>
2023-06-22 10:13:22 -0500

    feat: Add layout to InactiveRejected

--- src/components/inactive-rejected/inactive-rejected.js
+++ src/components/inactive-rejected/inactive-rejected.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import {
+  InputAdornment,
+  Paper,
+  Tab,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Tabs,
+  TextField,
+} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { EntitlementRestricted } from '../entitlement-restricted/entitlement-restricted';
+import { Layout } from '../layout/layout';
 
 export function InactiveRejected() {
   return (
-    <div>
-      <h1>Inactive Rejected</h1>
-    </div>
+    <EntitlementRestricted>
+      <Layout title="Inactive/Rejected">
+        <Tabs>
+          <Tab label="Student" />
+          <Tab label="Coach" />
+          <Tab label="Admin" />
+        </Tabs>
+        <TextField
+          placeholder="search..."
+          variant="outlined"
+          sx={{ mb: 2 }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment>
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>test</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>test</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Layout>
+    </EntitlementRestricted>
   );
 }
