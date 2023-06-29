a94b3a757539afb2d408aabd1c72dc18ef90e4f9
Holly R <hraetz14@gmail.com>
2023-06-29 10:34:56 -0500

    Fixed all modal imports

--- src/components/coaches/coaches.js
+++ src/components/coaches/coaches.js
@@ -5,7 +5,8 @@ import { Layout } from '../layout/layout';
 import { EntitlementRestricted } from '../entitlement-restricted/entitlement-restricted';
 import { getActiveCoaches } from '../../services/coaches/coaches';
 import { DynamicTableWithRequest } from '../table-layout/dynamicTableWithRequest';
-import { DeactivateCoachModal, GenericViewModal } from './modal-component';
+import { DeactivateCoachModal } from './deactivate-coach-modal';
+import { GenericViewModal } from '../shared/generic-view-modal';
 import { addCoachHandler } from './coachHandlers';
 import { getStudentsByCoachId } from '../../services/students/students';
 
