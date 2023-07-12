23a5449e7162b0fa14730da22ed680808dde74be
Om Rajpal <omrajpal15@gmail.com>
2023-07-12 09:27:49 -0500

    change button for add goal

--- src/components/student-details/goal-modals.js
+++ src/components/student-details/goal-modals.js
@@ -3,7 +3,7 @@ import EditIcon from '@mui/icons-material/Edit';
 import AddIcon from '@mui/icons-material/Add';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import dayjs from 'dayjs';
 import uuid from 'react-uuid';
 import propTypes from 'prop-types';
@@ -147,7 +147,7 @@ export function AddGoalModal(props) {
       cancelButtonTitle="Cancel"
       modalHeadingTitle="Add Goal"
       onActionButtonClick={requestSubmit}
-      openButtonIcon={<AddIcon />}
+      openButtonIcon={<Typography>Add Goal</Typography>}
     >
       <Grid
         container
