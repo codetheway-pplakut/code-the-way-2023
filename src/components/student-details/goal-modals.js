5888829a7316a0da48719e32cd402c20552fd801
Holly R <hraetz14@gmail.com>
2023-07-13 10:24:19 -0500

    fix

--- src/components/student-details/goal-modals.js
+++ src/components/student-details/goal-modals.js
@@ -8,7 +8,6 @@ import uuid from 'react-uuid';
 import propTypes from 'prop-types';
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
-import AddIcon from '@mui/icons-material/Add';
 
 import { GenericModal } from '../shared/generic-modal';
 import { TextFieldWithErrorMessage } from '../coaches/text-field-with-error-message';
