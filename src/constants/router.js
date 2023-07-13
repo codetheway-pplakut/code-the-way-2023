9697d87b36160753eefde568b30ac004ffe954de
Holly R <hraetz14@gmail.com>
2023-07-13 14:29:01 -0500

    Goals Clarity

--- src/constants/router.js
+++ src/constants/router.js
@@ -7,7 +7,6 @@ import { isProduction } from '../utils/is-production/is-production';
 import { Splash } from '../components/splash/splash';
 import { Students } from '../components/students/students';
 import { Coaches } from '../components/coaches/coaches';
-import { Goals } from '../components/goals/goals';
 import { InactiveRejected } from '../components/inactive-rejected/inactive-rejected';
 import { StudentInfo } from '../components/student-details/student-info';
 import { Admins } from '../components/admin/admin';
@@ -21,7 +20,6 @@ export const ROUTER = createBrowserRouter([
       { path: 'admins', element: <Admins /> },
       { path: 'coaches', element: <Coaches /> },
       { path: 'login', element: <Login /> },
-      { path: 'goals', element: <Goals /> },
       { path: 'students', element: <Students /> },
       { path: 'inactive-rejected', element: <InactiveRejected /> },
       { path: 'student-info', element: <StudentInfo /> },
