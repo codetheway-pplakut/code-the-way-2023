3b7bf29feed558894aea2c73694bea4402f44fb0
Aniket Singh <singani@hamiltonstudents.org>
2023-07-14 10:02:41 -0500

    feat: edit-parent-modal val

--- src/components/student-details/edit-student-info-modal.js
+++ src/components/student-details/edit-student-info-modal.js
@@ -6,6 +6,7 @@ import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
 import { validate } from 'validate.js';
+import { flattenDeep } from 'lodash';
 import { GenericModal } from '../shared/generic-modal';
 import { TextFieldWithErrorMessage } from '../shared/text-field-with-error-message';
 import { editStudent } from '../../services/students/students';
@@ -165,19 +166,73 @@ export function EditParentModal(props) {
   const [parentLastName, setParentLastName] = useState('');
   const [parentCellPhone, setParentCellPhone] = useState('');
   const [parentEmail, setParentEmail] = useState('');
-  const [address, setAddress] = useState('');
+  const [parentAddress, setParentAddress] = useState('');
   const [parentApartmentNumber, setParentApartmentNumber] = useState('');
   const [parentCity, setParentCity] = useState('');
   const [parentState, setParentState] = useState('');
   const [parentZipCode, setParentZipCode] = useState('');
   const [header, setHeader] = useState('');
 
+  const [parentFirstNameEdit, setParentFirstNameEdit] = useState(false);
+  const [parentLastNameEdit, setParentLastNameEdit] = useState(false);
+  const [parentCellPhoneEdit, setParentCellPhoneEdit] = useState(false);
+  const [parentEmailEdit, setParentEmailEdit] = useState(false);
+  const [parentAddressEdit, setParentAddressEdit] = useState(false);
+  const [parentApartmentNumberEdit, setParentApartmentNumberEdit] =
+    useState(false);
+  const [parentCityEdit, setParentCityEdit] = useState(false);
+  const [parentStateEdit, setParentStateEdit] = useState(false);
+  const [parentZipCodeEdit, setParentZipCodeEdit] = useState(false);
+
+  const validator = validate(
+    {
+      parentFirstName,
+      parentLastName,
+      parentEmail,
+      parentCellPhone,
+      parentAddress,
+      parentApartmentNumber,
+      parentCity,
+      parentState,
+      parentZipCode,
+    },
+    {
+      parentFirstName: {
+        presence: { allowEmpty: false, message: '' },
+      },
+      parentLastName: {
+        presence: { allowEmpty: false },
+      },
+      parentEmail: {
+        presence: { allowEmpty: false },
+        email: true,
+      },
+      parentCellPhone: {
+        presence: { allowEmpty: false },
+        format: {
+          pattern: '^([0-9]{3}){1}[-. ]?([0-9]{3}){1}[-. ]?([0-9]{4}){1}',
+        },
+      },
+      parentAddress: { presence: { allowEmpty: false, message: '' } },
+      parentApartmentNumber: { presence: { allowEmpty: false, message: '' } },
+      parentCity: { presence: { allowEmpty: false, message: '' } },
+      parentState: { presence: { allowEmpty: false, message: '' } },
+      parentZipCode: {
+        presence: { allowEmpty: false, message: '' },
+        format: { pattern: '^([0-9]{5}){1}', message: 'must be 5 digits' },
+      },
+    },
+    { fullMessages: false }
+  );
+
+  const messages = flattenDeep(Object.values(validator || {}));
+  const actionButtonDisabled = Boolean(messages.length);
   useEffect(() => {
     setParentFirstName(student.parentFirstName);
     setParentLastName(student.parentLastName);
     setParentCellPhone(student.parentCellPhone);
     setParentEmail(student.parentEmail);
-    setAddress(student.address);
+    setParentAddress(student.address);
     setParentApartmentNumber(student.parentApartmentNumber);
     setParentState(student.parentState);
     setParentCity(student.parentCity);
@@ -192,12 +247,22 @@ export function EditParentModal(props) {
       parentLastName,
       parentCellPhone,
       parentEmail,
-      address,
+      address: parentAddress,
       parentApartmentNumber,
       parentCity,
       parentState,
       parentZipCode,
     };
+
+    setParentFirstNameEdit(false);
+    setParentLastNameEdit(false);
+    setParentCellPhoneEdit(false);
+    setParentEmailEdit(false);
+    setParentAddressEdit(false);
+    setParentApartmentNumberEdit(false);
+    setParentCityEdit(false);
+    setParentStateEdit(false);
+    setParentZipCodeEdit(false);
 
     await editStudent(updatedParent);
 
@@ -210,77 +275,116 @@ export function EditParentModal(props) {
       cancelButtonTitle="cancel"
       modalHeadingTitle={header}
       onActionButtonClick={requestSave}
+      actionButtonDisabled={actionButtonDisabled}
       openModal={<EditIcon />}
     >
       <Grid container spacing={1} padding={4}>
         <Grid container item xs={12} justifyContent="center" spacing={2}>
           <Grid item xs={6}>
-            <TextFieldWithErrorMessage
+            <TextField
               label="First Name"
-              onChange={(value) => setParentFirstName(value)}
+              onChange={(event) => setParentFirstName(event.target.value)}
               value={parentFirstName}
+              errorText={parentFirstName.length < 1 ? 'Enter First Name' : ' '}
+              error={parentFirstName.length < 1 && parentFirstNameEdit}
+              required
+              onBlur={() => setParentFirstNameEdit(true)}
             />
           </Grid>
           <Grid item xs={6}>
-            <TextFieldWithErrorMessage
+            <TextField
               label="Last Name"
-              onChange={(value) => setParentLastName(value)}
+              onChange={(event) => setParentLastName(event.target.value)}
               value={parentLastName}
+              errorText={parentLastName.length < 1 ? 'Enter Last Name' : ' '}
+              error={parentLastName.length < 1 && parentLastNameEdit}
+              required
+              onBlur={() => setParentLastNameEdit(true)}
             />
           </Grid>
         </Grid>
         <Grid item xs={12}>
-          <TextFieldWithErrorMessage
+          <TextField
             fullWidth
             label="Preferred Phone Number"
-            onChange={(value) => setParentCellPhone(value)}
+            onChange={(event) => setParentCellPhone(event.target.value)}
             value={parentCellPhone}
+            errorText={parentCellPhone.length < 1 ? 'Enter Phone Number' : ' '}
+            error={parentCellPhone.length < 1 && parentCellPhoneEdit}
+            required
+            onBlur={() => setParentCellPhoneEdit(true)}
           />
         </Grid>
         <Grid item xs={12}>
-          <TextFieldWithErrorMessage
+          <TextField
             label="Email"
-            onChange={(value) => setParentEmail(value)}
+            onChange={(event) => setParentEmail(event.target.value)}
             value={parentEmail}
+            errorText={parentEmail.length < 1 ? 'Enter Email' : ' '}
+            error={parentEmail.length < 1 && parentEmailEdit}
+            required
+            onBlur={() => setParentEmailEdit(true)}
           />
         </Grid>
         <Grid container item xs={12} justifyContent="center" spacing={2}>
           <Grid item xs={8}>
-            <TextFieldWithErrorMessage
+            <TextField
               label="Address"
-              onChange={(value) => setAddress(value)}
-              value={address}
+              onChange={(event) => setParentAddress(event.target.value)}
+              value={parentAddress}
+              errorText={parentAddress.length < 1 ? 'Enter Address' : ' '}
+              error={parentAddress.length < 1 && parentAddressEdit}
+              required
+              onBlur={() => setParentAddressEdit(true)}
             />
           </Grid>
           <Grid item xs={4}>
-            <TextFieldWithErrorMessage
+            <TextField
               label="Apt."
-              onChange={(value) => setParentApartmentNumber(value)}
+              onChange={(event) => setParentApartmentNumber(event.target.value)}
               value={parentApartmentNumber}
+              errorText={parentApartmentNumber.length < 1 ? 'Enter Apt.' : ' '}
+              error={
+                parentApartmentNumber.length < 1 && parentApartmentNumberEdit
+              }
+              required
+              onBlur={() => setParentApartmentNumberEdit(true)}
             />
           </Grid>
         </Grid>
 
         <Grid container item xs={12} justifyContent="center" spacing={2}>
           <Grid item xs={4}>
-            <TextFieldWithErrorMessage
+            <TextField
               label="City"
-              onChange={(value) => setParentCity(value)}
+              onChange={(event) => setParentCity(event.target.value)}
               value={parentCity}
+              errorText={parentCity.length < 1 ? 'Enter City' : ' '}
+              error={parentCity.length < 1 && parentCityEdit}
+              required
+              onBlur={() => setParentCityEdit(true)}
             />
           </Grid>
           <Grid item xs={4}>
-            <TextFieldWithErrorMessage
+            <TextField
               label="State"
-              onChange={(value) => setParentState(value)}
+              onChange={(event) => setParentState(event.target.value)}
               value={parentState}
+              errorText={parentState.length < 1 ? 'Enter State' : ' '}
+              error={parentState.length < 1 && parentStateEdit}
+              required
+              onBlur={() => setParentStateEdit(true)}
             />
           </Grid>
           <Grid item xs={4}>
-            <TextFieldWithErrorMessage
+            <TextField
               label="Zip Code"
-              onChange={(value) => setParentZipCode(value)}
+              onChange={(event) => setParentZipCode(event.target.value)}
               value={parentZipCode}
+              errorText={parentZipCode.length < 1 ? 'Enter Zip Code' : ' '}
+              error={parentZipCode.length < 1 && parentZipCodeEdit}
+              required
+              onBlur={() => setParentZipCodeEdit(true)}
             />
           </Grid>
         </Grid>
