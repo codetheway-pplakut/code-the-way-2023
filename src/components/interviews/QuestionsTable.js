93e2b3622ee19bcbe3a584fad815713284f1f7c5
Adam Miller <theadammiller56@gmail.com>
2023-07-18 11:07:19 -0500

    Questions Table Display

--- src/components/interviews/QuestionsTable.js
+++ src/components/interviews/QuestionsTable.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import { DynamicTableWithRequest } from '../table-layout/dynamicTableWithRequest';
+import React, { useEffect, useState } from 'react';
 import { getInterviewHandler } from './interviewsHandler';
+import { DynamicTable } from '../table-layout/dynamicTable';
 
 const interviewId = '92ad7555-1de2-4c82-9cbb-1e24117f0626';
+
 const COLUMNS = [
   {
     id: 'questionString',
@@ -13,13 +14,39 @@ const COLUMNS = [
 ];
 
 export function QuestionsTable() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [rows, setRows] = useState([]);
+
+  const request = async () => {
+    setIsLoading(true);
+    setHasError(false);
+
+    try {
+      const response = await getInterviewHandler(interviewId).questions;
+      console.log(response);
+      const { data } = response;
+      setRows(data);
+      console.log(rows);
+    } catch (error) {
+      setRows([]);
+      setHasError(true);
+    }
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    request();
+  }, []);
+
   return (
     <React.Fragment>
       <h1>Interview</h1>
-      <DynamicTableWithRequest
-        columns={COLUMNS}
-        requestFunc={getInterviewHandler}
-        requestData={interviewId}
+      <DynamicTable
+        APIcolumns={COLUMNS}
+        APIrows={rows}
+        refreshTable={request}
       />
     </React.Fragment>
   );
