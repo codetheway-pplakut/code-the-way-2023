6108fba7482bb06a96b53bfc2a0fa1e38083d902
Brandon Moyer <brandon.a.moyer@gmail.com>
2023-07-19 13:55:30 -0500

    Revert "Merge branch 'statesDropdown' into develop"
    This reverts commit a8d9594b1ac17541d89235264bdabba17761f926.

--- src/components/student-details/edit-student-info-modal.js
+++ src/components/student-details/edit-student-info-modal.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { CircularProgress, Grid, MenuItem, TextField } from '@mui/material';
+import { CircularProgress, Grid, TextField } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
@@ -292,66 +292,13 @@ export default function EditStudentInfoModal(props) {
           <Grid item xs={4}>
             <TextField
               label="State"
-              select
               onChange={(event) => setStudentState(event.target.value)}
               value={studentState}
               helperText={displayErrorMessages('studentState')}
               error={checkError('studentState') && studentStateEdit}
               onBlur={() => setStudentStateEdit(true)}
               required
-              fullWidth
-            >
-              <MenuItem value="AL"> AL</MenuItem>
-              <MenuItem value="AK"> AK</MenuItem>
-              <MenuItem value="AZ"> AZ</MenuItem>
-              <MenuItem value="AR"> AR</MenuItem>
-              <MenuItem value="CA"> CA</MenuItem>
-              <MenuItem value="CO"> CO</MenuItem>
-              <MenuItem value="CT"> CT</MenuItem>
-              <MenuItem value="DE"> DE</MenuItem>
-              <MenuItem value="FL"> FL</MenuItem>
-              <MenuItem value="GA"> GA</MenuItem>
-              <MenuItem value="HI"> HI</MenuItem>
-              <MenuItem value="ID"> ID</MenuItem>
-              <MenuItem value="IL"> IL</MenuItem>
-              <MenuItem value="IN"> IN</MenuItem>
-              <MenuItem value="IA"> IA</MenuItem>
-              <MenuItem value="KS"> KS</MenuItem>
-              <MenuItem value="KY"> KY</MenuItem>
-              <MenuItem value="LA"> LA</MenuItem>
-              <MenuItem value="ME"> ME</MenuItem>
-              <MenuItem value="MD"> MD</MenuItem>
-              <MenuItem value="MA"> MA</MenuItem>
-              <MenuItem value="MI"> MI</MenuItem>
-              <MenuItem value="MN"> MN</MenuItem>
-              <MenuItem value="MS"> MS</MenuItem>
-              <MenuItem value="MO"> MO</MenuItem>
-              <MenuItem value="MT"> MT</MenuItem>
-              <MenuItem value="NE"> NE</MenuItem>
-              <MenuItem value="NV"> NV</MenuItem>
-              <MenuItem value="NH"> NH</MenuItem>
-              <MenuItem value="NJ"> NJ</MenuItem>
-              <MenuItem value="NM"> NM</MenuItem>
-              <MenuItem value="NY"> NY</MenuItem>
-              <MenuItem value="NC"> NC</MenuItem>
-              <MenuItem value="ND"> ND</MenuItem>
-              <MenuItem value="OH"> OH</MenuItem>
-              <MenuItem value="OK"> OK</MenuItem>
-              <MenuItem value="OR"> OR</MenuItem>
-              <MenuItem value="PA"> PA</MenuItem>
-              <MenuItem value="RI"> RI</MenuItem>
-              <MenuItem value="SC"> SC</MenuItem>
-              <MenuItem value="SD"> SD</MenuItem>
-              <MenuItem value="TN"> TN</MenuItem>
-              <MenuItem value="TX"> TX</MenuItem>
-              <MenuItem value="UT"> UT</MenuItem>
-              <MenuItem value="VT"> VT</MenuItem>
-              <MenuItem value="VA"> VA</MenuItem>
-              <MenuItem value="WA"> WA</MenuItem>
-              <MenuItem value="WV"> WV</MenuItem>
-              <MenuItem value="WI"> WI</MenuItem>
-              <MenuItem value="WY"> WY</MenuItem>
-            </TextField>
+            />
           </Grid>
           <Grid item xs={4}>
             <TextField
@@ -540,17 +487,15 @@ export function EditParentModal(props) {
   };
 
   const handleClose = () => {
-    setParentFirstName(student.parentFirstName ? student.parentFirstName : '');
-    setParentLastName(student.parentLastName ? student.parentLastName : '');
-    setParentCellPhone(student.parentCellPhone ? student.parentCellPhone : '');
-    setParentEmail(student.parentEmail ? student.parentEmail : '');
-    setParentAddress(student.address ? student.address : '');
-    setParentApartmentNumber(
-      student.parentApartmentNumber ? student.parentApartmentNumber : ''
-    );
-    setParentCity(student.parentCity ? student.parentCity : '');
-    setParentState(student.parentState ? student.parentState : '');
-    setParentZipCode(student.parentZipCode ? student.parentZipCode : '');
+    setParentFirstName(student.parentFirstName);
+    setParentLastName(student.parentLastName);
+    setParentCellPhone(student.parentCellPhone);
+    setParentEmail(student.parentEmail);
+    setParentAddress(student.address);
+    setParentApartmentNumber(student.parentApartmentNumber);
+    setParentCity(student.parentCity);
+    setParentState(student.parentState);
+    setParentZipCode(student.parentZipCode);
 
     setParentFirstNameEdit(false);
     setParentLastNameEdit(false);
@@ -662,66 +607,13 @@ export function EditParentModal(props) {
           <Grid item xs={4}>
             <TextField
               label="State"
-              select
               onChange={(event) => setParentState(event.target.value)}
               value={parentState}
               helperText={displayErrorMessages('parentState')}
               error={checkError('parentState') && parentStateEdit}
               required
-              fullWidth
               onBlur={() => setParentStateEdit(true)}
-            >
-              <MenuItem value="AL"> AL</MenuItem>
-              <MenuItem value="AK"> AK</MenuItem>
-              <MenuItem value="AZ"> AZ</MenuItem>
-              <MenuItem value="AR"> AR</MenuItem>
-              <MenuItem value="CA"> CA</MenuItem>
-              <MenuItem value="CO"> CO</MenuItem>
-              <MenuItem value="CT"> CT</MenuItem>
-              <MenuItem value="DE"> DE</MenuItem>
-              <MenuItem value="FL"> FL</MenuItem>
-              <MenuItem value="GA"> GA</MenuItem>
-              <MenuItem value="HI"> HI</MenuItem>
-              <MenuItem value="ID"> ID</MenuItem>
-              <MenuItem value="IL"> IL</MenuItem>
-              <MenuItem value="IN"> IN</MenuItem>
-              <MenuItem value="IA"> IA</MenuItem>
-              <MenuItem value="KS"> KS</MenuItem>
-              <MenuItem value="KY"> KY</MenuItem>
-              <MenuItem value="LA"> LA</MenuItem>
-              <MenuItem value="ME"> ME</MenuItem>
-              <MenuItem value="MD"> MD</MenuItem>
-              <MenuItem value="MA"> MA</MenuItem>
-              <MenuItem value="MI"> MI</MenuItem>
-              <MenuItem value="MN"> MN</MenuItem>
-              <MenuItem value="MS"> MS</MenuItem>
-              <MenuItem value="MO"> MO</MenuItem>
-              <MenuItem value="MT"> MT</MenuItem>
-              <MenuItem value="NE"> NE</MenuItem>
-              <MenuItem value="NV"> NV</MenuItem>
-              <MenuItem value="NH"> NH</MenuItem>
-              <MenuItem value="NJ"> NJ</MenuItem>
-              <MenuItem value="NM"> NM</MenuItem>
-              <MenuItem value="NY"> NY</MenuItem>
-              <MenuItem value="NC"> NC</MenuItem>
-              <MenuItem value="ND"> ND</MenuItem>
-              <MenuItem value="OH"> OH</MenuItem>
-              <MenuItem value="OK"> OK</MenuItem>
-              <MenuItem value="OR"> OR</MenuItem>
-              <MenuItem value="PA"> PA</MenuItem>
-              <MenuItem value="RI"> RI</MenuItem>
-              <MenuItem value="SC"> SC</MenuItem>
-              <MenuItem value="SD"> SD</MenuItem>
-              <MenuItem value="TN"> TN</MenuItem>
-              <MenuItem value="TX"> TX</MenuItem>
-              <MenuItem value="UT"> UT</MenuItem>
-              <MenuItem value="VT"> VT</MenuItem>
-              <MenuItem value="VA"> VA</MenuItem>
-              <MenuItem value="WA"> WA</MenuItem>
-              <MenuItem value="WV"> WV</MenuItem>
-              <MenuItem value="WI"> WI</MenuItem>
-              <MenuItem value="WY"> WY</MenuItem>{' '}
-            </TextField>
+            />
           </Grid>
           <Grid item xs={4}>
             <TextField
