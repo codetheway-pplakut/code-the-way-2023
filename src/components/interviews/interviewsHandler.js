937cd2f5661a86240334cdf93c3374274e6fec8d
Adam Miller <theadammiller56@gmail.com>
2023-07-19 09:22:33 -0500

    New Interview API calls

--- src/components/interviews/interviewsHandler.js
+++ src/components/interviews/interviewsHandler.js
@@ -1,17 +1,56 @@
 import {
-  createStudentResponse,
+  CreateStudentAnswers,
+  GetInterviewAndQuestions,
+  addInterview,
+  deleteInterview,
+  editAnswer,
+  editInterview,
   getInterview,
-  getStudentResponse,
+  getInterviews,
+  getStudentInterviewResponses,
 } from '../../services/interviews/interviews';
+
+export function getInterviewsHandler() {
+  return getInterviews();
+}
 
 export function getInterviewHandler(interviewId) {
   return getInterview(interviewId);
 }
 
-export function getStudentResponseHandler(studentId) {
-  return getStudentResponse(studentId);
+export function getInterviewAndQuestionsHandler(interviewId) {
+  return GetInterviewAndQuestions(interviewId);
 }
 
-export function createStudentResponseHandler(studentResponse) {
-  return createStudentResponse(studentResponse);
+export function addInterviewHandler(interviewName) {
+  const data = { interviewName };
+  return addInterview(data);
+}
+
+export function editInterviewHandler(questions, interviewName, interviewId) {
+  const data = { questions, interviewName, id: interviewId };
+  return editInterview(data);
+}
+
+export function deleteInterviewHandler(interviewId) {
+  return deleteInterview(interviewId);
+}
+
+export function getStudentInterviewResponsesHandler(studentId, interviewId) {
+  return getStudentInterviewResponses(studentId, interviewId);
+}
+
+export function CreateStudentAnswersHandler(
+  answerString,
+  studentId,
+  questionId,
+  interviewId
+) {
+  const data = { answerString, studentId, questionId, interviewId };
+  return CreateStudentAnswers(data);
+}
+
+export function editAnswerHandler(answerString, id) {
+  const data = { answerString, id };
+  return editAnswer(data);
 }
--- src/services/interviews/interviews.js
+++ src/services/interviews/interviews.js
@@ -1,19 +1,37 @@
 import { callApi } from '../../utils/call-api/call-api';
 
-export const getInterview = async (interviewId) =>
-  callApi({ url: `/Interviews/${interviewId}` });
+export const getInterviews = () =>
+  callApi({ url: '/Interviews/GetInterviews' });
 
-export const getStudentResponse = (studentId) =>
-  callApi({ url: `/Interviews/student-responses/${studentId}` });
+export const getInterview = (interviewId) =>
+  callApi({ url: `/Interviews/GetInterview/${interviewId}` });
 
-/**
- *
- * @param {[{answerString:string, id:UUID, studentId:UUID, questionId:UUID, questionString:string, itnerviewId:UUID, isArchived:boolean, studentName:string, interviewName:string}]} studentResponse
- * @returns
- */
-export const createStudentResponse = (studentResponse) =>
+export const GetInterviewAndQuestions = (interviewId) =>
+  callApi({ url: `/Interviews/GetInterviewAndQuestions/${interviewId}` });
+
+export const addInterview = (interview) =>
+  callApi({ url: '/Interviews', data: interview, method: 'POST' });
+
+export const editInterview = (interview) =>
+  callApi({ url: '/Interviews', data: interview, method: 'PUT' });
+
+export const deleteInterview = (interviewId) =>
+  callApi({ url: '/Interviews', params: interviewId, method: 'DELETE' });
+
+export const getStudentInterviewResponses = (studentId, interviewId) =>
   callApi({
-    url: '/Interviews/student-responses',
-    data: studentResponse,
+    url: `/Interviews/GetStudentInterviewResponses/${studentId}/${interviewId}`,
+  });
+
+export const CreateStudentAnswers = (answer) =>
+  callApi({
+    url: '/Interviews/CreateStudentAnswers',
+    data: answer,
     method: 'POST',
   });
+
+export const editAnswer = (answer) =>
+  callApi({ url: '/Answers', data: answer, method: 'PUT' });
+
+export const deleteAnswer = (answerId) =>
+  callApi({ url: '/Answers', params: answerId, method: 'DELETE' });
