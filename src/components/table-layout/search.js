1cf5443f85576df120ab6c1daa839cd8fde4faf8
Jihong <jihongbae2nd@gmail.com>
2023-07-19 13:18:27 -0500

    seachbar half not click is clicking

--- src/components/table-layout/search.js
+++ src/components/table-layout/search.js
@@ -36,7 +36,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     transition: theme.transitions.create('width'),
     width: '100%',
     [theme.breakpoints.up('md')]: {
-      width: '20ch',
+      width: '47.5ch',
     },
   },
 }));
