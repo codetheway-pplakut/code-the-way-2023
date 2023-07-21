0783505ff8b51730a4274a0527c07dbd978a0f8b
Aniket Singh <singani@hamiltonstudents.org>
2023-07-21 09:12:39 -0500

    fix: password api fixes

--- src/components/login/forgot-password-modal.js
+++ src/components/login/forgot-password-modal.js
@@ -2,84 +2,32 @@ import React, { useEffect, useState } from 'react';
 import { Button, Grid, TextField, Typography } from '@mui/material';
 import { validate } from 'validate.js';
 import { flattenDeep } from 'lodash';
-import sgMail from '@sendgrid/mail';
+
 import GenericModal from '../shared/generic-modal';
 import { requestPasswordReset } from '../../services/users/users';
-import {
-  getActiveAdminsHandler,
-  getInactiveAdminsHandler,
-} from '../admin/adminHandlers';
-import {
-  getActiveCoachesHandler,
-  getInactiveCoachesHandler,
-} from '../coaches/coachHandlers';
 
 export function ForgotPasswordModal() {
   const [email, setEmail] = React.useState('');
   const [emailEdit, setEmailEdit] = React.useState(false);
   const [token, setToken] = React.useState('');
   const [open, setOpen] = React.useState(false);
-  const [activeCoach, setActiveCoach] = useState([]);
-  const [inactiveCoach, setInactiveCoach] = useState([]);
-  const [activeAdmin, setActiveAdmin] = useState([]);
-  const [inactiveAdmin, setInactiveAdmin] = useState([]);
+  const [error, setError] = React.useState(false);
 
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-  const requestActiveCoaches = async () => {
-    const response = await getActiveCoachesHandler();
-    const { data } = response;
-    setActiveCoach(data);
-  };
-
-  const requestInactiveCoaches = async () => {
-    const response = await getInactiveCoachesHandler();
-    const { data } = response;
-    setInactiveCoach(data);
-  };
-
-  const requestActiveAdmins = async () => {
-    const response = await getActiveAdminsHandler();
-    const { data } = response;
-    setActiveAdmin(data);
-  };
-
-  const requestInactiveAdmins = async () => {
-    const response = await getInactiveAdminsHandler();
-    const { data } = response;
-    setInactiveAdmin(data);
-  };
-
-  const coachEmailList = (arr1, arr2, arr3, arr4) => {
-    const value1 = arr1.map((val) => val.coachEmail);
-    const value2 = arr2.map((val) => val.coachEmail);
-    const value3 = arr3.map((val) => val.email);
-    const value4 = arr4.map((val) => val.email);
-    const finalValue = flattenDeep([value1, value2, value3, value4]);
-    return finalValue;
-  };
-
-  useEffect(() => {
-    requestActiveCoaches();
-    requestInactiveCoaches();
-    requestActiveAdmins();
-    requestInactiveAdmins();
-  }, []);
   const validator = validate(
     { email },
     {
       email: {
         email: true,
         presence: { allowEmpty: false, message: ' ' },
-        inclusion: {
-          within: coachEmailList(
-            activeCoach,
-            inactiveCoach,
-            activeAdmin,
-            inactiveAdmin
-          ),
-          message: 'This email is not used',
-        },
+        // inclusion: {
+        //   within: coachEmailList(
+        //     activeCoach,
+        //     inactiveCoach,
+        //     activeAdmin,
+        //     inactiveAdmin
+        //   ),
+        //   message: 'This email is not used',
+        // },
       },
     },
     { fullMessages: false }
@@ -102,30 +50,18 @@ export function ForgotPasswordModal() {
   };
   const submitAction = async () => {
     const data = { email };
-    const response = await requestPasswordReset(data);
-    const unslicedToken = response.data['Return URL'];
-    setToken(unslicedToken.slice('51'));
-    localStorage.setItem('token', token);
-    console.log(token);
-    console.log('http://localhost:8080/reset-password');
-    const msg = {
-      to: 'test@example.com',
-      from: 'email', // Use the email address or domain you verified above
-      subject: 'Reset Password',
-      text: `${token} http://localhost:8080/reset-password`,
-    };
-    setOpen(true);
-    (async () => {
-      try {
-        await sgMail.send(msg);
-      } catch (error) {
-        console.error(error);
-
-        if (error.response) {
-          console.error(error.response.body);
-        }
-      }
-    })();
+    try {
+      setError(false);
+      const response = await requestPasswordReset(data);
+      const unslicedToken = response.data['Return URL'];
+      setToken(unslicedToken.slice('51'));
+      localStorage.setItem('token', token);
+      console.log(token);
+      console.log('http://localhost:8080/reset-password');
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
   };
   const messages = flattenDeep(Object.values(validator || {}));
   const actionButtonDisabled = Boolean(messages.length);
@@ -154,10 +90,16 @@ export function ForgotPasswordModal() {
       </GenericModal>
       <Grid>
         <Grid item xs={1}>
-          {' '}
           <Typography>{token}</Typography>
         </Grid>
       </Grid>
+      {error && (
+        <Grid>
+          <Grid item xs={1}>
+            <Typography> Error Occured, Please Try Again</Typography>
+          </Grid>
+        </Grid>
+      )}
     </React.Fragment>
   );
 }
